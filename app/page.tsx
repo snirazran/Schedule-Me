0bac5b19b40863f224d9eaee1e4e4715682ea95c
Snir Azran <snirazran1@gmail.com>
2025-04-29 23:36:06 +0300

    push

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button';
+export default function Home() {
+  return (
+    <>
+      <div>hello world</div>
+      <Button>Default</Button>
+    </>
+  );
+}
